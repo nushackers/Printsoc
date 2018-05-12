d881ed407ad583f3dd2b7e2be7f806c4530c555f
E-Liang Tan <eliang@eliangtan.com>
2018-05-12 14:42:09 +0800

    Upgrade react-navigation

--- App/Navigation/ReduxNavigation.js
+++ App/Navigation/ReduxNavigation.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BackHandler, Platform } from 'react-native';
-import { addNavigationHelpers } from 'react-navigation';
 import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
 import { connect } from 'react-redux';
 import AppNavigation from './AppNavigation';
@@ -28,11 +27,11 @@ class ReduxNavigation extends React.Component {
   render() {
     return (
       <AppNavigation
-        navigation={addNavigationHelpers({
+        navigation={{
           dispatch: this.props.dispatch,
           state: this.props.nav,
           addListener: createReduxBoundAddListener('root')
-        })}
+        }}
       />
     );
   }
